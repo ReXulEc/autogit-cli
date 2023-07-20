1613c921a1447cfcdd05c206250159a0e2f78e01
Mert Doğu <mertdoguonder@protonmail.com>
2023-07-20 14:58:42 +0300

    feat: add strict mode for token resets

--- util/checkToken.js
+++ util/checkToken.js
@@ -1,7 +1,14 @@
 const crypto = require('crypto');
 
-const checkToken = (json) => {
+const checkToken = (json, strict, log) => {
     let temp = json
+
+    if (strict) {
+        return new Promise((resolve, reject) => {
+            temp.token = crypto.randomBytes(64).toString('hex');
+            resolve(true, temp);
+        });
+    }
     if(json.token?.length === 128 && json.token){
         return new Promise((resolve, reject) => {
             resolve(true);
